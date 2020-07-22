d170aa3dc56ae1ba812970ca26ceb8162e3e496d
chirummamillagowthamkumar <chirummamillagowtham333@gmail.com>
2020-07-22 12:33:01 +0530

    give any three numbers it check result is zero or not

---
+++ zero.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+echo "enter 1st number"
+read a
+echo "enter 2nd number"
+read b
+echo "enter 3rd number"
+read c
+ 
+z=$(( $a+$b+$c ))
+    echo $z 
+w=$(( $z-$z ))
+echo $w
+if [ $z -eq 0 ]
+then
+     echo  "number is zero"
+else
+      echo "invalid"
+fi
